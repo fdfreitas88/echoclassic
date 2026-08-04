57bda99f9779264e3b8e4ef2b4355da867892e96
Felipe Freitas <64909764+fdfreitas88@users.noreply.github.com>
2026-08-03 23:14:04 -0300

    release: 3.2.1 — o funil, o painel e as facetas que faltavam

--- EchoClassic/Plugin.pm
+++ EchoClassic/Plugin.pm
@@ -44,7 +44,7 @@ sub initPlugin {
 
 sub getDisplayName { return 'ECHOCLASSIC_SKIN' }
 
-sub getSkinVersion { return '3.2.0' }
+sub getSkinVersion { return '3.2.1' }
 
 sub jsLiteral {
 	my $v = defined $_[0] ? "$_[0]" : '';
